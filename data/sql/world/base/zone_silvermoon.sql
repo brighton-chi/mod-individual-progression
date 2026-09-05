0552060fac01d536e05148042a337d2349c38dba
Grimfeather <88028633+Grimfeather@users.noreply.github.com>
2026-09-05 23:52:05 +0200

    Update zone_silvermoon.sql

--- data/sql/world/base/zone_silvermoon.sql
+++ data/sql/world/base/zone_silvermoon.sql
@@ -8,14 +8,13 @@ UPDATE `creature_template` SET `rank` = 3, `type_flags` = 4 WHERE `entry` = 1754
 
 -- Restore Blood Knight quests to pre-2.4 state
 DELETE FROM `quest_template` WHERE `ID` IN (9681, 9684, 109681, 109684);
-INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `RewardNextQuest`, `RewardXPDifficulty`, `StartItem`, `Flags`, `AllowableRaces`, 
-`LogTitle`, `QuestCompletionLog`, `RequiredItemId1`, `RequiredItemCount1`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `RewardNextQuest`, `RewardXPDifficulty`, `StartItem`, `Flags`, `AllowableRaces`,
+`LogTitle`, `QuestCompletionLog`, `RequiredItemId1`, `RequiredItemCount1`, `VerifiedBuild`) VALUES
+--
 (9681,   2, -1, 12, -141, 9684, 3, 0, 136, 512, 'A Study in Power', NULL, 0, 0, 12340),
 (9684,   2, -1, 12, -141, 9685, 3, 24157, 128, 512, 'Claiming the Light', 'Return to Knight-Lord Bloodvalor in Eversong Woods.', 24156, 1, 12340),
 (109681, 2, -1, 12, -141, 9684, 3, 0, 136, 512, 'A Study in Power', NULL, 0, 0, 12340),
 (109684, 2, -1, 12, -141, 9685, 3, 24157, 128, 512, 'Claiming the Light', 'Return to Knight-Lord Bloodvalor in Eversong Woods.', 24156, 1, 12340);
-
-UPDATE `quest_template_addon` SET `NextQuestID` = 9685 WHERE `ID` IN (9684, 109684);
 
 UPDATE `quest_template` SET 
 `QuestDescription` = 'Do not think me unduly harsh, $N. Stillblade knew well what he was being sent to do. He understood and accepted his duty and is an example for all aspiring Blood Knights.$B$BMy words are no eulogy, $N. Stillblade\'s service to us is far from over and you will be the instrument of his resurrection.$B$BYour education in the order\'s ways has just begun. With the betrayal of Kael\'thas and the loss of our power source, you must learn new methods of wielding the Light. Seek out Magister Astalor Bloodsworn.'
@@ -35,14 +34,18 @@ UPDATE `quest_template` SET
 `QuestDescription` = "Take this vessel, $N. Invoking its magic will allow you to take this creature's power and claim it for yourself. With it, you will be able to restore your fallen comrade to life.$B$BThere are those who would say what we're doing is wrong, but much of what has befallen us is also wrong. Would they have us throw away such a gift?$B$BThey do not complain when our power is used to prevent the further destruction of Silvermoon. No, they object when they are safe from harm and indulging their magic addictions."
 WHERE `ID` = 109684;
 
-DELETE FROM `quest_template_addon` WHERE `ID` IN (109681, 109684);
+DELETE FROM `quest_template_addon` WHERE `ID` IN (9681, 9684, 109681, 109684);
 INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `SourceSpellID`, `PrevQuestID`, `NextQuestID`, `ExclusiveGroup`, `BreadcrumbForQuestId`, `RewardMailTemplateID`, `RewardMailDelay`, 
-`RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES 
+`RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES
+--
+(9681, 0, 2, 0, 9678, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(9684, 0, 2, 0, 9681, 9685, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
 (109681, 0, 2, 0, 9678, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 (109684, 0, 2, 0, 109681, 9685, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
 
 DELETE FROM `quest_template_locale` WHERE `ID` IN (109681, 109684);
-INSERT INTO `quest_template_locale` (`ID`, `locale`, `Title`, `Details`, `Objectives`, `EndText`, `CompletedText`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_template_locale` (`ID`, `locale`, `Title`, `Details`, `Objectives`, `EndText`, `CompletedText`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4`, `VerifiedBuild`) VALUES
+--
 (109681, 'deDE', 'Eine Studie der Macht', 'Haltet mich nicht für übermäßig grausam, $n. Stillstreich hat sehr wohl gewusst, worauf er sich einlässt. Er verstand und akzeptierte seine Pflicht und wurde so ein leuchtendes Vorbild für alle aufstrebenden Blutritter.$B$BMeine Worte sind nicht als Grabesrede gedacht, $n. Stillstreichs Dienste für uns gehören noch lange nicht der Vergangenheit an und Ihr werdet das Instrument seiner Wiederauferstehung sein.$B$BEure Ausbildung in der Kunst des Ordens hat gerade erst begonnen. Angesichts Kael\'thas\' Verrat und des Verlusts unserer Energiequelle, müsst Ihr neue Wege zur Beherrschung des Lichts erlernen. Sucht Magister Astalor Blutschwur auf.', 'Sprecht mit Magister Astalor Blutschwur in der Geheimen Kammer unter dem Hauptquartier der Blutelfen.', '', '', '', '', '', '', 18019),
 (109681, 'esES', 'Un estudio sobre el poder', 'No creas que no lo siento, $n. Hojafirme sabía bien a lo que se enfrentaba. Aceptó su deber y es un ejemplo para todo aspirante a Caballero de sangre.$B$BNo hablo por hablar, $n. Hojafirme sigue a nuestro servicio y tú ayudarás a resucitarlo.$B$BHasta ahora, tu instrucción sobre la orden acaba de empezar. Tras la traición de Kael\'thas y la pérdida de nuestra fuente de poder, debes aprender nuevos métodos para ejercer la Luz. Busca al magister Astalor Jurasangre.', 'Habla con el magister Astalor Jurasangre en la cámara oculta debajo del cuartel general de los Caballeros de sangre.', '', '', '', '', '', '', 18019),
 (109681, 'esMX', 'Un estudio sobre el poder', 'No creas que no lo siento, $n. Hojafirme sabía bien a lo que se enfrentaba. Aceptó su deber y es un ejemplo para todo aspirante a Caballero de sangre.$B$BNo hablo por hablar, $n. Hojafirme sigue a nuestro servicio y tú ayudarás a resucitarlo.$B$BHasta ahora, tu instrucción sobre la orden acaba de empezar. Tras la traición de Kael\'thas y la pérdida de nuestra fuente de poder, debes aprender nuevos métodos para ejercer la Luz. Busca al magister Astalor Jurasangre.', 'Habla con el magister Astalor Jurasangre en la cámara oculta debajo del cuartel general de los Caballeros de sangre.', '', '', '', '', '', '', 18019),
@@ -57,7 +60,7 @@ INSERT INTO `quest_template_locale` (`ID`, `locale`, `Title`, `Details`, `Object
 (109684, 'zhCN', '驾驭圣光', '拿着这只瓶子，$N。启动其中的魔法，你就可以吸取这些魔导师身上残存的光明能量。他们曾经负责控制那个被我们捕获的纳鲁，因此纳鲁的能量还有一部分留在他们体内。$B$B这应该就足以让你的伙伴起死回生了。$B$B不用为这些魔导师担心，$N。我正在照料他们，而且索拉纳·血怒大人向我保证过，莉亚德琳大人正在积极寻找新的能量源。', '用血精灵魔导师身上的能量装满微光容器，然后向银月城的骑士领主布拉德瓦罗复命。', '', '去找永歌森林的骑士领主布拉德瓦罗。', '', '', '', '', 0);
 
 DELETE FROM `quest_request_items_locale` WHERE `ID` = 109684;
-INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `VerifiedBuild`) VALUES
 (109684, 'deDE', 'Habt Ihr schon mit Magister Astalor Blutschwur gesprochen?', 18019),
 (109684, 'esES', '¿Has hablado con el Magister Astalor Jurasangre?', 0),
 (109684, 'esMX', '¿Has hablado con el Magister Astalor Jurasangre?', 0),
@@ -65,18 +68,18 @@ INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `Ver
 (109684, 'zhCN', '你和博学者阿斯塔洛·血誓谈过了吗？', 0);
 
 DELETE FROM `quest_request_items` WHERE `ID` IN (109681, 109684);
-INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `CompletionText`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `CompletionText`, `VerifiedBuild`) VALUES
 (109681, 1, 0, NULL, 12340),
 (109684, 1, 0, 'Have you spoken with Magister Astalor Bloodsworn?', 12340);
 
 DELETE FROM `quest_poi` WHERE `QuestID` IN (109681, 109684);
-INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES
 (109681, 0, -1, 530, 462, 0, 0, 1, 0),
 (109684, 0, -1, 530, 462, 0, 0, 1, 0),
 (109684, 1, 4, 530, 462, 0, 0, 1, 0);
 
 DELETE FROM `quest_poi_points` WHERE `QuestID` IN (109681, 109684);
-INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES
 (109681, 0, 0, 9861, -7516, 0),
 (109684, 0, 0, 9870, -7482, 0),
 (109684, 1, 0, 9848, -7527, 0),
@@ -85,12 +88,13 @@ INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBu
 (109684, 1, 3, 9845, -7523, 0);
 
 DELETE FROM `quest_offer_reward` WHERE `ID` IN (109681, 109684);
-INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_offer_reward` (`ID`, `Emote1`, `Emote2`, `Emote3`, `Emote4`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `EmoteDelay4`, `RewardText`, `VerifiedBuild`) VALUES
 (109681, 0, 0, 0, 0, 0, 0, 0, 0, 'Welcome, young $c.$B$BThe power of the Blood Knights is taken from a being of immense power.$B$B<The magister gestures to the large, captive being in the center of the room.>$B$BIsn\'t it magnificent? \'Twas a gift from our beloved Prince Kael\'thas, and it is the foundation of the Blood Knights\' mastery of the Light. This creature begrudges us its power, so we have devised a method for claiming it on our own terms.', 12340),
 (109684, 0, 0, 0, 0, 0, 0, 0, 0, 'Well done, $N. The powers granted by the Light will never come \'naturally\' to the Blood Knights, but we are not so naive as to be thwarted by the will of a single being, however powerful.$B$BOur mastery of the arcane has unlocked this path and these powers for you, initiate. Few have the stomach or the aptitude to wield them, but I know you will not disappoint the order.', 12340);
 
 DELETE FROM `quest_offer_reward_locale` WHERE `ID` IN (109681, 109684);
-INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES 
+INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) VALUES
+--
 (109681, 'deDE', 'Willkommen, junger $C.$B$BAls der Orden gegründet wurde, machte uns Kael\'thas einen gefangenen Naaru zum Geschenk. Dieses Wesen besteht aus genau dem Licht, das wir beherrschen. Mithilfe unserer überragenden magischen Fähigkeiten lernten wir, wie man es kontrolliert und unterwirft, damit sich die Blutritter seiner Energie bedienen können.$B$BDoch Kael\'thas wurde von seiner eigenen Machtgier übermannt und verbündete sich mit unseren Feinden. Seine niederträchtigen Anhänger griffen uns an, nahmen den Naaru und schnitten uns von der Energiequelle ab. Unser Orden erlebt dunkle Zeiten, $N.', 18019),
 (109681, 'esES', '$gBienvenido:Bienvenida;, joven $n.$B$BEl poder de los Caballeros de Sangre se toma de un ser de inmenso poder.$B$B<El magister señala al gran ser cautivo que está en el centro de la habitación.>$B$B¿No es magnífico? Fue un regalo de nuestro amado Príncipe Kael\'thas, y es la base del dominio de la Luz de los Caballeros de Sangre. Esta criatura nos limita su poder, por lo que hemos ideado un método para reclamarlo en nuestros propios términos.', 0),
 (109681, 'esMX', '$gBienvenido:Bienvenida;, joven $n.$B$BEl poder de los Caballeros de Sangre se toma de un ser de inmenso poder.$B$B<El magister señala al gran ser cautivo que está en el centro de la habitación.>$B$B¿No es magnífico? Fue un regalo de nuestro amado Príncipe Kael\'thas, y es la base del dominio de la Luz de los Caballeros de Sangre. Esta criatura nos limita su poder, por lo que hemos ideado un método para reclamarlo en nuestros propios términos.', 0),
@@ -103,18 +107,18 @@ INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `Verified
 (109684, 'zhCN', '做得好，$N。圣光所给予的力量永远不会是血骑士与生俱来的，但我们也不会软弱到任由命运来宰制我们。$B$B我们对秘法专精提供了这条道路，来帮助我们度过这段困难的时光。力量不再自由地弥漫于这些殿堂之间，但我们还是有坚定而强壮的意志力。如今很少有人有这样的兴趣与天赋来钻研此道，但我相信你不会让骑士团失望的。', 0);
 
 DELETE FROM `creature_queststarter` WHERE `quest` IN (109681, 109684);
-INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES 
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
 (17717, 109681),
 (17718, 109684);
 
 DELETE FROM `creature_questender` WHERE `quest` IN (109681, 109684);
-INSERT INTO `creature_questender` (`id`, `quest`) VALUES 
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
 (17717, 109684),
 (17718, 109681);
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 19 AND `ConditionTypeOrReference` = 8 AND `SourceEntry` IN (9681, 9684, 109681, 109684);
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
-`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`,
+`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (19, 0,   9681, 0, 0, 8, 0, 66012, 0, 0, 0, 0, 0, '', 'WotLK Blood Knight quest only available after TBC stage 4'),
 (19, 0,   9684, 0, 0, 8, 0, 66012, 0, 0, 0, 0, 0, '', 'WotLK Blood Knight quest only available after TBC stage 4'),
 (19, 0, 109681, 0, 0, 8, 0, 66012, 0, 0, 1, 0, 0, '', 'pre-2.4 Blood Knight quest only available before TBC stage 5'),
@@ -132,8 +136,8 @@ UPDATE `creature_addon` SET `bytes1` = 0, `bytes2` = 0, `emote` = 0 WHERE `guid`
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 17845;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (-96982, -96980, -96981, -96979, -96978) AND `source_type` = 0 AND `action_param1` = 31324;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, 
-`event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, 
-`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, 
+`event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`,
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`,
 `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 --
 (-96982, 0, 0, 0, 1, 0, 100, 0, 1000, 1000, 0, 0, 0, 11, 31324, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Blood Elf Magister - Out of Combat - Cast Drain Naaru Channel'),
@@ -159,8 +163,8 @@ UPDATE `conditions` SET `ConditionValue2` = 17544 WHERE `SourceTypeOrReferenceId
 
 -- Restore Lady Liadrin and move Magister Astalor Bloodsworn a bit.
 DELETE FROM `creature` WHERE `guid` IN (96976, 617076);
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, 
-`wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES 
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`,
+`wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
 (96976,  17718, 530, 0, 0, 1, 1, 0, 9859.45, -7519.19, -8.06573, 1.69273, 180, 0, 0, 3484, 5751, 0, 0, 0, 0, '', 0, 0, NULL),        -- Magister Astalor Bloodsworn
 (617076, 17076, 530, 0, 0, 1, 1, 0, 9862.12, -7518.44, -8.06524, 1.86945, 300, 0, 0, 1214000, 33870, 0, 0, 0, 0, '', NULL, 0, NULL); -- Lady Liadrin
 
@@ -168,6 +172,6 @@ UPDATE `creature_template` SET `gossip_menu_id` = 0 WHERE `entry` = 17718; -- wa
 
 -- Hide 'Lor'Themar's gossip menu until WotLK
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 7632;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, 
-`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`,
+`ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (14, 7632, 9315, 0, 0, 8, 0, 66013, 0, 0, 0, 0, 0, '', 'Lor\'Themar - only show Gossip Menu after player reaches WotLK');
